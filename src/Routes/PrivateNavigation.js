17919006e96b29d854a411693ec8bb44242958ab
Herrynto2 <herryzen02@gmail.com>
2020-04-03 01:59:26 +0700

    layout home

--- src/Routes/PrivateNavigation.js
+++ src/Routes/PrivateNavigation.js
@@ -5,18 +5,20 @@ import ProfileUpdate from '../Screens/Profile/ProfileUpdate';
 import TopupNavigate from './Components/TopupNavigate';
 import Header from '../Screens/Profile/Component/Header';
 import OnProgress from '../Components/OnProgress';
+import Method from '../Screens/Home/Components/Method';
 
 function PrivateNavigation(props) {
   const Stack = createStackNavigator();
   return (
     <Stack.Navigator
       screenOptions={{
-        headerShown: false,
+        headerShown: false
       }}>
       <Stack.Screen name="Main" component={Main} />
       <Stack.Screen name="TopupNavigate" component={TopupNavigate} />
       <Stack.Screen name="ProfileUpdate" component={ProfileUpdate} />
       <Stack.Screen name="Header" component={Header} />
+      <Stack.Screen name="Method" component={Method} />
       <Stack.Screen name="OnProgress" component={OnProgress} />
     </Stack.Navigator>
   );
