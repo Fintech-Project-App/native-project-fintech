ca4e5a02e3d909660ccac4fe1aad2aa93eabfa6a
Alen1945 <dwialendraipangselly23@gmail.com>
2020-04-05 22:27:33 +0700

    done handle on internet

--- App.js
+++ App.js
@@ -9,18 +9,31 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './src/Redux/store';
 import SplashScreen from 'react-native-splash-screen';
-
+import NetInfo from '@react-native-community/netinfo'
+import { Toast, Root } from 'native-base'
 function App(props) {
   React.useEffect(() => {
     SplashScreen.hide();
+    NetInfo.fetch().then(state => {
+      if (!state.isConnected) {
+        Toast.show({
+          text: 'No Internet Connection',
+          buttonText: 'Ok',
+          duration: 15000,
+          position: 'bottom',
+          type: "warning"
+        })
+      }
+    });
   }, []);
-
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Routes />
-      </PersistGate>
-    </Provider>
+    <Root>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Routes />
+        </PersistGate>
+      </Provider>
+    </Root>
   );
 }
 export default App;
